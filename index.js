874fdd24466f4f439e4c0d0302c80f8d53d2bde4
user01 <user01@user01s-MacBook-Air.local>
2022-06-21 14:42:59 +0900

    config

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const{User} = require('./models/User');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:admin@boilerplate.w3ircbo.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('Mongo DB Connected'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', (req, res) => {  res.send('Hello refresh')})
 
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true,
+        })
+    })
+  })
+  
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
 
 //npm run start -> localhost:3000
